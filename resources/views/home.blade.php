a9da5cca4b0aa86f93472dd7e10e28ed812c7ea0
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-07-22 23:03:41 +0530

    modify

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,5 @@
     <x-slot:heading> 
         Home Page
     </x-slot:heading>
-
-    @foreach
-<li><strong>{{$job['title']}}:</strong>pays {{$job['salary']}}per year.</li>
-    @endforeach
-    
-<h1>hello from the Home page</h1>
+  
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,54 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/home', function () {
+    return view('home');
 });
 Route::get('/contact', function () {
     return view('contact');
 });
-Route::get('/home', function () {
-    return view('home');
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => [
+        [
+            'id' => 1,
+           'title' => 'Director',
+           'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$20,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$10,000'
+        ]
+        ] 
+    ]);
+});
+Route::get('/jobs/{id}', function ($id) {
+    $jobs=[
+        [
+            'id' => 1,
+           'title' => 'Director',
+           'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$20,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '$10,000'
+        ]
+        
+    ];
+    
+    $job= \Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+    dd($job);   
+    return view('jobs');
 });
