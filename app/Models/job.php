04be525c394e6f9e13b1c6a3fef1ab2468d353bf
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-07-25 22:08:13 +0530

    modify

--- app/Models/job.php
+++ app/Models/job.php
@@ -8,5 +8,9 @@ class Job extends Model{
 
     protected $table = 'job_listings';
     protected $fillable = ['title','salary'];
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
 }
 ?>
