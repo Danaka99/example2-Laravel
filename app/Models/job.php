26b684327d13119504ecb5ce5d480a5fed91646a
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-07-26 16:00:58 +0530

    modify

--- app/Models/job.php
+++ app/Models/job.php
@@ -3,6 +3,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+
 class Job extends Model{
     use HasFactory;
 
@@ -11,6 +12,10 @@ class Job extends Model{
 
     public function employer(){
         return $this->belongsTo(Employer::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listings_id"); 
     }
 }
 ?>
