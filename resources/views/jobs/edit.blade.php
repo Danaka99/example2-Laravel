65c14fb25d82fa50b4a9584b7aa02219e8717b00
Danaka99 <140838401+Danaka99@users.noreply.github.com>
2024-07-28 21:42:45 +0530

    modify

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -2,7 +2,7 @@
 <x-slot:heading> 
        Edit Job {{ $job->title }}
     </x-slot:heading> 
-<form method="Post" action="/jobs/{{ $job->id }}" >
+<form method="POST" action="/jobs/{{ $job->id }}" >
     @csrf
     @method('PATCH')
   <div class="space-y-12">
@@ -41,28 +41,28 @@
             @error('salary')
              <p class="text-xs text-red-500 font-semibold mt-1"> {{ $message}}</p>
             @enderror
+            </div>
           </div>
-        </div>
+       </div>
+      </div>
     </div>
-    <!-- <div class="mt-10">@if($errors->any())
-    <ul>
-     @foreach($errors->all() as $error)
-       <li class="text-red-500 italic">{{ $error  }}</li>
-     @endforeach
-     </ul>
-     @endif
-    </div> -->
   </div>
- </div>
-<div class="mt-6 flex items-center justify-end gap-x-6">
-  <div></div>
-  <div>
-  <a href="/jobs/{{ $job->id }}">
-    </button class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-  </a>
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+  <div class="mt-6 flex items-center justify-between gap-x-6"> 
+    <div class="flex items-center">
+      <button form=delete-form class="text-red-500 text-sm font-bold">Delete</button>
+    </div>
+    <div class="mt-6 flex items-center gap-x-6">
+     <a href="/jobs/{{ $job->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel </a>
+     <div>
+     <button type="submit" 
+     class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+    </div>
+   </div>
   </div>
-  </div>
+</form>
+<form method="POST" action="/jobs/{{ $job->id }}" id="delete-form" class ="hiden">
+    @csrf
+    @method('DELETE')
 </form>
 
  
